859ec93b0c1cb556592c9e5e3aa862654306f622
tami <mabutami@apple.com>
2023-02-06 21:59:20 +0200

    update how to play

--- ui.py
+++ ui.py
@@ -122,10 +122,17 @@ def main():
 
         """
         ℹ️ How To Play?\n
-            Find the secret word. You have unlimited guesses.
-            The words were sorted by an artificial intelligence algorithm according to how similar they were to the secret word.
-            After submitting a word, you will see its position. The secret word is number 1.
-            The algorithm analyzed thousands of texts. It uses the context in which words are used to calculate the similarity between them.
+        Find the secret word.
+        You have unlimited guesses.
+        The words were sorted 
+        by an artificial intelligence algorithm 
+        according to how similar they were to the secret word.
+        After submitting a word,
+        you will see its position.
+        The secret word is number 1.
+        The algorithm analyzed thousands of texts.
+        It uses the context in which words
+         are used to calculate the similarity between them.
         """
 
 
